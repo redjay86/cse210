79cbe039bc2ee1e77e32954db7a440b28188a912
RexWight <rexjared86@gmail.com>
2025-06-28 13:28:48 -0600

    W03 Prove complete

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference refe = new Reference("2 Nephi", 31, 11, 12);
+        string text = "And the Father said: Repent ye, repent ye, and be baptized in the name of my Beloved Son. And also, the voice of the Son came unto me, saying: He that is baptized in my name, to him will the Father give the Holy Ghost, like unto me; wherefore, follow me, and do the things which ye have seen me do.";
+        Scripture scripture = new Scripture(refe, text);
+
+        string response;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            response = Console.ReadLine();
+            scripture.HideRandomWords(3);
+        } while (!scripture.IsCompletelyHidden() && response != "quit");
+        if (scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+        }
     }
 }
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,59 @@
+class Scripture
+{
+    Reference _reference;
+    List<Word> _words = [];
+
+    public Scripture(Reference refe, string text)
+    {
+        _reference = refe;
+
+        foreach (string word in text.Split(" ").ToList())
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < numberToHide; i++)
+        {
+            if (IsCompletelyHidden())
+            {
+                break;
+            }
+            while (true)
+            {
+                int idx = rand.Next(_words.Count);
+                Word randWord = _words[idx];
+                if (!randWord.IsHidden())
+                {
+                    randWord.Hide();
+                    break;
+                }
+            }
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public string GetDisplayText()
+    {
+        string res = "";
+        foreach (Word word in _words)
+        {
+            res += word.GetDisplayText() + " ";
+        }
+        return _reference.GetDisplayText() + res;
+    }
+}
